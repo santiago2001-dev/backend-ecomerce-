e1f6b651eeebb4fd5863e22c650a7eb2a5f1e19d
victor morales <63662613+santiago2001-dev@users.noreply.github.com>
2023-02-07 20:27:08 -0500

    ruta

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server{
         this.app = express();
         this.port = process.env.PORT || 3000;
         this.loginPath =  '/api/login';
-        this.usersPath = '/api/users';
+        this.usersPath = '/';
         this.productPath  = '/api/product';
         this.ventasPath = '/api/ventas'
         //ruta de nuesta apirest
